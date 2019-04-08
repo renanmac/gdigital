4dc6fd742b887e6d3d26bf6487dda92d56910c7d
allangdy <allangdy2594@gmail.com>
2019-04-08 17:10:17 -0300

    'feito_testes'

--- test/actions/newlead/index_test.js
+++ test/actions/newlead/index_test.js
@@ -1,33 +1,30 @@
 const plg = require('pluga-plg');
 const expect = require('chai').expect;
-
 const action = require('../../../lib/actions/newlead/');
 
+
 const event = {
-  meta: {
-    baseURI: 'https://testespark.gpages.com.br/api/pluga/newlead.php'
-  },
-  auth: {
-    id: '15738',
-    tk: process.env.API_SECRET
-  },
-  input: {
-    email: 'all12394@gmail.com',
-    nome: 'Teste Cadastro',
-    telefone: '44999915204',
-    acao: 'cadastrar',
-    id_form: '9405'
-  },
-  
+	meta: {
+		baseURI: process.env.BASE_URI
+	},
+	auth: {
+		ID: process.env.API_KEY,
+		tk: process.env.API_SECRET
+	},
+	input: {
+		email: 'testeemail@gmail.com',
+		nome: 'Teste Pluga2',
+		telefone: '44999999999',
+		acao: 'cadastrar',
+		id_form: '9405',
+	},  
 };
 
 describe('Action: New lead', function () {
-  it('Deveria criar novo lead', function (done) {
-    action.handle(plg, event).then((res) => {
-      console.log(res);
-      console.log(event);
-      console.log(process.env);
-      done();
-    }).catch(done);
-  });
+	it('Deveria criar novo lead', function (done) {
+		action.handle(plg, event).then((res) => {
+			console.log(res);
+			done();
+		}).catch(done);
+  	});
 });
--- test/test_helper.js
+++ test/test_helper.js
@@ -6,16 +6,13 @@
  * - [chai](https://chaijs.com)
  */
 
-if (process.env.AXIOS_DEBUG) {
-	require('dotenv').config();
+if (process.env.AXIOS_DEBUG) {	
   const plg = require('pluga-plg');
   const axiosDebug = require('axios-debug');
   axiosDebug(plg.axios);
-
 }
 
 before(function (done) {
-  console.log('Testing functions...');
-  console.log(process.env);
+  console.log('Testing functions...'); 
   done();
 });
